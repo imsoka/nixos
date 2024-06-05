4c6fa78013c7d87588887a0e77e8bf17699fdb56
Victor Araque Casaus <Vctor Araque Casaus>
2024-06-05 10:32:44 +0200

    remove 'config.'

--- nixos/hosts/personal/applications.nix
+++ nixos/hosts/personal/applications.nix
@@ -16,17 +16,17 @@
       ../../applications/graphical/browsers/firefox.nix
     ];
 
-  config.nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 
-  config.git.enable = true;
-  config.firefox.enable = true;
-  config.neovim.enable = true;
-  config.vim.enable = true;
-  config.home-manager.enable = true;
-  config.nextcloud-client.enable = true;
-  config.keepassxc.enable = true;
-  config.lutris.enable = true;
-  config.steam.enable = true;
-  config.webcord.enable = true;
-  config.gparted.enable = true;
+  git.enable = true;
+  firefox.enable = true;
+  neovim.enable = true;
+  vim.enable = true;
+  home-manager.enable = true;
+  nextcloud-client.enable = true;
+  keepassxc.enable = true;
+  lutris.enable = true;
+  steam.enable = true;
+  webcord.enable = true;
+  gparted.enable = true;
 }
