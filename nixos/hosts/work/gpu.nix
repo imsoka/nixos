10875cbd572bb9ebb8ff25cc169dc22ea37c14f2
Víctor Araque <victor.araque@ogilvy.com>
2025-01-28 00:14:18 +0100

    add nvidia drivers

--- nixos/hosts/work/gpu.nix
+++ nixos/hosts/work/gpu.nix
@@ -3,4 +3,26 @@
   hardware.graphics = {
     enable = true;
   };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+
+    open = false;
+
+    nvidiaSettings = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    prime = {
+      sync.enable = true;
+    
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1.0.0";
+    };
+  };
 }
